ea6b863de95f6baeffa9df0883c590faae239610
PragatiVerma18 <itispragativerma@gmail.com>
2020-06-27 13:08:17 +0530

    added css transitions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import logo from "./logo.svg";
-import Question from "./Components/Question";
+import Quiz from "./Components/Quiz";
+import quizQuestions from "./api/quizQuestions";
+import Result from "./Components/Result";
 class App extends Component {
   constructor(props) {
     super(props);
@@ -15,20 +17,118 @@ class App extends Component {
       answersCount: {},
       result: "",
     };
+
+    this.handleAnswerSelected = this.handleAnswerSelected.bind(this);
+  }
+
+  componentDidMount() {
+    const shuffledAnswerOptions = quizQuestions.map((question) =>
+      this.shuffleArray(question.answers)
+    );
+
+    this.setState({
+      question: quizQuestions[0].question,
+      answerOptions: shuffledAnswerOptions[0],
+    });
+  }
+
+  shuffleArray(array) {
+    var currentIndex = array.length,
+      temporaryValue,
+      randomIndex;
+
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+  }
+
+  handleAnswerSelected(event) {
+    this.setUserAnswer(event.currentTarget.value);
+
+    if (this.state.questionId < quizQuestions.length) {
+      setTimeout(() => this.setNextQuestion(), 300);
+    } else {
+      setTimeout(() => this.setResults(this.getResults()), 300);
+    }
+  }
+
+  setUserAnswer(answer) {
+    this.setState((state, props) => ({
+      answersCount: {
+        ...state.answersCount,
+        [answer]: (state.answersCount[answer] || 0) + 1,
+      },
+      answer: answer,
+    }));
+  }
+
+  setNextQuestion() {
+    const counter = this.state.counter + 1;
+    const questionId = this.state.questionId + 1;
+
+    this.setState({
+      counter: counter,
+      questionId: questionId,
+      question: quizQuestions[counter].question,
+      answerOptions: quizQuestions[counter].answers,
+      answer: "",
+    });
+  }
+
+  getResults() {
+    const answersCount = this.state.answersCount;
+    const answersCountKeys = Object.keys(answersCount);
+    const answersCountValues = answersCountKeys.map((key) => answersCount[key]);
+    const maxAnswerCount = Math.max.apply(null, answersCountValues);
+
+    return answersCountKeys.filter(
+      (key) => answersCount[key] === maxAnswerCount
+    );
+  }
+
+  setResults(result) {
+    if (result.length === 1) {
+      this.setState({ result: result[0] });
+    } else {
+      this.setState({ result: "Undetermined" });
+    }
+  }
+
+  renderQuiz() {
+    return (
+      <Quiz
+        answer={this.state.answer}
+        answerOptions={this.state.answerOptions}
+        questionId={this.state.questionId}
+        question={this.state.question}
+        questionTotal={quizQuestions.length}
+        onAnswerSelected={this.handleAnswerSelected}
+      />
+    );
+  }
+
+  renderResult() {
+    return <Result quizResult={this.state.result} />;
   }
 
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img
-            src="https://media.giphy.com/media/YqukXc3tzSUoL4bgQg/giphy.gif"
-            className="App-logo"
-            alt="logo"
-          />
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>React Quiz</h2>
         </div>
-        <h1>React Quiz</h1>
-        <Question content="What is your favourite food?" />
+        {this.state.result ? this.renderResult() : this.renderQuiz()}
       </div>
     );
   }
